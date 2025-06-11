9be2fa9a41c208d1956d5c4b279f654073d56210
Yonathan Nieves <yonathannieves17@gmail.com>
2025-06-10 22:05:02 -0500

    first commit

---
+++ resources/views/layouts/auth-style.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Recuperación de contraseña</title>
+    <link rel="stylesheet" href="{{ asset('build/assets/css/recuperar.css') }}">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('logo/icon.png') }}">
+</head>
+<body>
+    <section>
+        <div class="contenedor">
+            @yield('content')
+        </div>
+    </section>
+</body>
+</html>
